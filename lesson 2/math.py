f14370e1d09126b2ba1293bac2e57d9920cfea59
Kozakdi <hrosudiana7@gmail.com>
2022-04-13 11:41:06 +0300

    Create math.py

---
+++ lesson 2/math.py
@@ -0,0 +1,37 @@
+print('2 + 2 =', 2 + 2)
+print('10 - 3 =', 10 - 3)
+
+print('10 * 10 =', 10 * 10)
+print('20 / 2 =', 20 / 2)
+print('5 / 2 =', 5 / 2)
+
+print('5 // 2 =', 5 // 2) # ділення з результатом типу int
+print(type(5 // 2), int(5 / 2)) # int(5 / 2) - еквівалентний результат
+
+
+print('2 + 2 * 2 =', 2 + 2 * 2)
+print('(2 + 2) * 2 =', (2 + 2) * 2)
+
+
+print('5 % 2 =', 5 % 2) # modulo, 2 + 2 + (1) = 5
+print('11 % 4 =', 11 % 4) # 4 + 4 +(3) = 11
+print(10 % 2)
+print(9 % 2)
+
+
+# Якщо зі строками
+print('Hello' + 'Cucumber')
+print('Hello' + 3) # не вийде
+print('Hello' / 'o') # не вийде
+print(')' * 100)
+print('lol' + ')' * 100)
+
+# З булевими
+print(True + True)
+print(False + True)
+print(True + True + True + True)
+
+# Робимо від'ємні числа
+num = 33
+print(-num)
+
