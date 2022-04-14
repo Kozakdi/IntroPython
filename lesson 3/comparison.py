ebd3f5ed4ac7006444c4edbedaf9a56e3a566bac
Kozakdi <hrosudiana7@gmail.com>
2022-04-14 12:32:56 +0300

    Create comparison.py

---
+++ lesson 3/comparison.py
@@ -0,0 +1,23 @@
+print(1 == 1)
+print(1 == 2)
+print('Cucumber' == 'Cucumber')
+print('cucumber' == 'Cucumber') # це різні рядки
+print(1 != 'Cucumber')
+
+print(10 > 5)
+print(10 < 5)
+
+print(5 >= 5)
+
+print(True == True)
+print(False == False)
+
+print('u' in 'Cucumber')
+print('bla' in 'blablabla')
+print('bla' in 'Cucumber')
+print('mu' in 'Cucumber')
+
+print(3 in '123') # так не вийде, буде помилка
+print(str(3) in '123')
+print(1 in 123) # так не можна
+
