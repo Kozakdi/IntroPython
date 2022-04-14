8a13e46a349ceb12f9e6564d8aef8703bbee3297
Kozakdi <hrosudiana7@gmail.com>
2022-04-14 14:38:17 +0300

    Create none.py

---
+++ lesson 3/none.py
@@ -0,0 +1,9 @@
+nothing = None
+print(nothing)
+print(bool(None)) # False
+print(not None) # True
+print(None == False) # False
+print(None == True) # False
+print(None == 0) # False
+print(nothing == None) # True
+ 
