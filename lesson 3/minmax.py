e0fae64f9605e3a7fbd685b2d3ba6a9f2908e2c2
Kozakdi <hrosudiana7@gmail.com>
2022-04-14 16:17:35 +0300

    Create minmax.py

---
+++ lesson 3/minmax.py
@@ -0,0 +1,14 @@
+a = 2
+b = 5
+
+print(min(a, b))
+print(max(a, b))
+
+c = 10
+print(max(a, b, c))
+
+val = 20
+val = max(0, min(val, 10)) # обмеження значення між 0 та 10
+print(val)
+
+help(min) # відобразити справку 
