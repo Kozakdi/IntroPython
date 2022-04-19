6669683a4adbe0785d18bebc8dfdb2779a3ee702
Kozakdi <hrosudiana7@gmail.com>
2022-04-19 19:49:52 +0300

    Create find_cucumber.py

---
+++ lesson 4/find_cucumber.py
@@ -0,0 +1,12 @@
+print('Зараз знайдемо огірок 🥒')
+text = input('Введіть фразу зі словом "cucumber": ')
+if 'cucumber' in text:
+    print(text[text.index('cucumber'):])
+else:
+    print('"cucumber" не знайдено....Ну блін, я ж просив😢')
+
+
+
+
+
+
