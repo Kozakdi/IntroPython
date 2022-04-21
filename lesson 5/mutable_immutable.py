2ab6859ef0835c7036bcf5ac632d9ba7f2e5fc20
Kozakdi <hrosudiana7@gmail.com>
2022-04-21 16:28:02 +0300

    Create mutable_immutable.py

---
+++ lesson 5/mutable_immutable.py
@@ -0,0 +1,18 @@
+numbers = [1, 2, 3]  # mutable
+numbers_copy = numbers
+
+print(numbers is numbers_copy)
+print(numbers)
+print(numbers_copy)
+
+numbers.append(4)
+
+print(numbers is numbers_copy)
+print(numbers)
+print(numbers_copy)
+
+numbers = (1, 2, 3)  # immutable
+numbers += (4, 5)
+print(numbers is numbers_copy)
+print(numbers)
+print(numbers_copy)
