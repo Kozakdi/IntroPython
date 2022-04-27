eea3f9105dfcce360264a12ed5f62c55d09d7354
Kozakdi <hrosudiana7@gmail.com>
2022-04-27 22:15:15 +0300

    Create calculator 0.3.py

---
+++ lesson 5/calculator_0.3.py
@@ -0,0 +1,27 @@
+print('Ласкаво просимо до неймовірного калькулятора (версія 0.3)')
+sym = input('Оберіть операцію (+, -, *, /, +++): ')
+if sym == '+' or sym == '-' or sym == '/' or sym == '*':
+    num = int(input('Введіть перше число: '))
+    num2 = int(input('Введіть друге число: '))
+    if sym == '+':
+        print(num, '+', num2, '=', num + num2)
+    elif sym == '-':
+        print(num, '-', num2, '=', num - num2)
+    elif sym == '*':
+        print(num, '*', num2, '=', num * num2)
+    elif sym == '/':
+        try:
+            num/num2
+        except ZeroDivisionError:
+            print('∞')
+            quit()
+    print(num, '/', num2, '=', num / num2)
+if sym == '+++':
+    num3 = None
+    numbers = []
+    while num3 != ' ':
+        num3 = input('Введіть число: ')
+        numbers.append(num3)
+        for number in range(len(numbers[:-1])):
+            numbers[number] = int(numbers[number])
+    print(sum(numbers[:-1]))
