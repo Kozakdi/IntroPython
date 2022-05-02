8caac75d79879ffafe5c3d014bd8a69d5f34dce0
Kozakdi <hrosudiana7@gmail.com>
2022-05-02 20:03:30 +0300

    Create sets.py

--- lesson 5/calculator_0.3.py
+++ lesson 5/calculator_0.3.py
@@ -11,19 +11,19 @@ if sym == '+' or sym == '-' or sym == '/' or sym == '*':
         print(num, '*', num2, '=', num * num2)
     elif sym == '/':
         try:
-            num/num2
+            (num, '/', num2, '=', num / num2)
         except ZeroDivisionError:
             print('∞')
             quit()
-    print(num, '/', num2, '=', num / num2)
-if sym == '+++':
+        print(num, '/', num2, '=', num / num2)
+elif sym == '+++':
     num3 = None
     numbers = []
     while num3 != ' ':
         num3 = input('Введіть число: ')
         numbers.append(num3)
-        for number in range(len(numbers[:-1])):
-            numbers[number] = int(numbers[number])
+    for number in range(len(numbers[:-1])):
+        numbers[number] = int(numbers[number])
     print(sum(numbers[:-1]))
 else:
     print('Помилка!')
